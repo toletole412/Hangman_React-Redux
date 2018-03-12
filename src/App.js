21d8afe8e1c053ed477cf8cdbcaaac92d7eef7b9
toletole412 <yoonjioh90@gmail.com>
2018-03-12 23:42:01 +0100

    render CreateGameButton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Board from './containers/Board'
+import CreateGameButton from './components/CreateGameButton'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="actions">
+          <CreateGameButton />
+        </div>
         <Board />
       </div>
     )
