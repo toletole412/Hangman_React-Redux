7f76679656bf74ef733bb9370d468458a12910e8
toletole412 <yoonjioh90@gmail.com>
2018-03-13 12:37:20 +0100

    Beautify Board using gingerman

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react'
 import Title from '../components/Title'
+import { connect } from 'react-redux'
+import { wrongGuessCount } from '../lib/game'
 import './Board.css'
 
 class Board extends PureComponent {
@@ -8,20 +10,41 @@ class Board extends PureComponent {
       <div>
         <Title content="Ginger hangman" />
         <ul className="left" id="head">
-            (<img src="https://imageshack.com/a/img924/43/v3X4aD.gif" alt="head"/>)
+          { wrongGuessCount(this.props.word, this.props.guesses) === 1 ?
+            (<img src="https://imageshack.com/a/img924/43/v3X4aD.gif" alt="head"/>) : ''
           }
         </ul>
         <ul className="left" id="arms">
-          <img src="https://imageshack.com/a/img922/1454/UvzGnf.gif" alt="body"/>
-          <img src="https://imageshack.com/a/img924/2823/KxbKw7.gif" alt="body"/>
+          { wrongGuessCount(this.props.word, this.props.guesses) === 2 ?
+            (<img src="https://imageshack.com/a/img922/1454/UvzGnf.gif" alt="body"/>) : ''
+          }
         </ul>
-        <ul className="left" id="foots">
-          <img src="https://imageshack.com/a/img924/3186/qlYVlo.gif" alt="foot"/>
-          <img src="https://imageshack.com/a/img923/8052/A8U1Ew.gif" alt="foot"/>
+        <ul className="right" id="arms">
+          { wrongGuessCount(this.props.word, this.props.guesses) === 3 ?
+            (<img src="https://imageshack.com/a/img924/2823/KxbKw7.gif" alt="body"/>) : ''
+          }
+        </ul>
+        <ul className="left" id="arms">
+          { wrongGuessCount(this.props.word, this.props.guesses) === 4 ?
+            (<img src="https://imageshack.com/a/img924/3186/qlYVlo.gif" alt="foot"/>) : ''
+          }
+        </ul>
+        <ul className="right" id="foots">
+          { wrongGuessCount(this.props.word, this.props.guesses) === 5 ?
+            (<img src="https://imageshack.com/a/img923/8052/A8U1Ew.gif" alt="foot"/>) : ''
+          }
+        </ul>
+
+        <ul className="left" id="whole">
+          { wrongGuessCount(this.props.word, this.props.guesses) === 6 ?
+            (<img src="https://imagizer.imageshack.com/v2/1024x768q90/923/x5uF2K.jpg" alt="foot"/>) : ''
+          }
         </ul>
       </div>
     )
   }
 }
 
-export default Board
+const mapStateToProps = ({word, guesses }) => ({ word, guesses})
+
+export default connect(mapStateToProps)(Board)
